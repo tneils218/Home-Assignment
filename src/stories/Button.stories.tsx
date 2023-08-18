537d5e87a74118fc0b0f404ee4abd8200107549e
tneils218 <trantrungnhat2512@gmail.com>
2023-08-18 15:47:48 +0700

    fix(*): UI storybook with can change label from ui

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,26 +1,45 @@
-import { Source, Story } from "@storybook/blocks";
-import { Meta, type StoryObj } from "@storybook/react";
-import { Button } from "./Button";
+import { Source, Story } from '@storybook/blocks'
+import { Meta, type StoryObj } from '@storybook/react'
+import { ButtonProps, Group } from '@mantine/core'
+import { MantineButton } from '../component/Button/MantineButton' // Correct import path
+import { IconBrush } from '@tabler/icons-react'
+import { Button } from './Button'
 
 const meta = {
-  component: Button,
-} satisfies Meta<typeof Button>;
+  component: Button
+} satisfies Meta<typeof Button>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
-export const WithLayout: Story = {
-  parameters: {
-    layout: "centered",
-    docs: {
-      page: () => (
-        <>
-          <Story />
-          <Source
-            language="tsx"
-            code={`
+export const WithLayout: Story = (args: ButtonProps) => (
+  <Group position='center' spacing='xs'>
+    <MantineButton
+      children={args.children}
+      leftIcon={<IconBrush size='1rem' />}
+      radius='sm'
+      variant='default'
+      color='grey'
+    />
+    <MantineButton children='Copy code' color='dark' variant='filled' radius='sm' />
+  </Group>
+)
+
+WithLayout.args = {
+  children: 'Customize'
+}
+
+WithLayout.parameters = {
+  layout: 'centered',
+  docs: {
+    page: () => (
+      <>
+        <Story />
+        <Source
+          language='tsx'
+          code={`
 import { Group } from "@mantine/core";
-import { MatineButton } from "../component/Button/MatineButton";
+import { MantineButton } from "../component/Button/MantineButton";
 import { IconBrush } from "@tabler/icons-react";
             
 export const Button = () => {
@@ -37,18 +56,17 @@ export const Button = () => {
         color="dark"
         variant="filled"
         radius="sm"
-        />
+      />
     </Group>
   );
 };
                 `}
-          />
-        </>
-      ),
-    },
-  },
-  tags: ["autodocs"],
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
-};
+        />
+      </>
+    )
+  }
+}
+WithLayout.tags = ['autodocs']
+WithLayout.argTypes = {
+  children: { control: 'text' }
+}
