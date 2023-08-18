b07e6aecfe4ea4f87e333f1d5e33bb50b0765fa1
tneils218 <trantrungnhat2512@gmail.com>
2023-08-18 13:58:00 +0700

    set up prettier for everybody

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,30 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
+    "eslint-config-prettier",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "prettier/prettier": [
+      "warn",
+      {
+        arrowParens: "always",
+        semi: false,
+        trailingComma: "none",
+        tabWidth: 2,
+        endOfLine: "auto",
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true,
+      },
     ],
   },
 };
